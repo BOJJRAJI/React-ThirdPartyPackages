3788f1dec7b5a169cad1420fe515f6bb6e250d52
BOJJA RAJI <94085980+BOJJRAJI@users.noreply.github.com>
2023-05-18 11:32:22 +0530

    Update index.js

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,14 +1,35 @@
+import {Component} from 'react'
 import ReactPlayer from 'react-player'
 
 import './index.css'
 
-const VideoPlayer = () => (
-  <div className="video-container">
-    <h1 className="heading">Video Player</h1>
-    <div className="responsive-container">
-      <ReactPlayer url="https://www.youtube.com/watch?v=T0BcSV2Suiw" />
-    </div>
-  </div>
-)
+class VideoPlayer extends Component {
+  state = {isPlay: false}
+
+  toggleVideo = () => {
+    this.setState(prevState => ({isPlay: !prevState.isPlay}))
+  }
+
+  render() {
+    const {isPlay} = this.state
+    return (
+      <div className="video-container">
+        <h1 className="heading">Video Player</h1>
+        <div className="responsive-container">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=T0BcSV2Suiw"
+            playing={isPlay}
+            height={300}
+            width={700}
+          />
+          <button onClick={this.toggleVideo} type="button" className="button">
+            PlayVideo
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default VideoPlayer
+
